698b935023b348484a5c006c16668e4507816cdd
Vinod Kumar KP <vinod.kunum-purath@capgemini.com>
2026-03-21 20:22:23 -0600

    Fixed the look

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  corePlugins: {
+    preflight: false, // Important: Keep this false so Docusaurus doesn't break!
+  },
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
     "./docs/**/*.{md,mdx}",
